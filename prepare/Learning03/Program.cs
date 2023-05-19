819952d445b0c2de29de2e9ed7e3211822b41822
taran-serhii <serhiitaran47@gmail.com>
2023-05-19 11:04:27 +0300

    modified Program file according to the assingment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction (6);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        //Getting the initial values with set up getter and displaying them
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+
+        // Changing the values using the setters
+        fraction1.SetTop(2);
+        fraction1.SetBottom(3);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+
     }
 }
